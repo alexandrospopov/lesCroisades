644af70c732119a25ac9819bfdf65ab92b1c9c6a
Alexandros Popov <alexandros.ppv@gmail.com>
2020-04-16 13:32:12 +0200

    map 1st implementation ok.

---
+++ src/map/map.js
@@ -0,0 +1,16 @@
+console.log("bonjour  ")
+
+
+var map = new GMaps({
+  el: '#googleMap',
+  lat:  40.730610,
+  lng: -73.935242
+});
+
+map.addMarker({
+  lat: 40.700610,
+  lng: -73.997242,
+  title: 'New York'
+});
+
+map.setZoom(8);
