4dd737d0647f476b9d514cc96f19007966eeba97
Alexandros Popov <alexandros.ppv@gmail.com>
2020-04-20 16:01:44 +0200

    start to work on fusion.

--- src/d3/tripOverlay.js
+++ src/d3/tripOverlay.js
@@ -12,8 +12,8 @@ function initialize(){
     var map = new google.maps.Map(document.getElementById("googleMap"),myOptions);
     var marker = new google.maps.Marker({position:myLatLng,map:map});
     marker.setMap(map);
-    var swBound = new google.maps.LatLng(28.51031, -12.03265 );
-    var neBound = new google.maps.LatLng(49.07932, 43.12375);
+    var swBound = new google.maps.LatLng( 28.51031, -12.03265 );
+    var neBound = new google.maps.LatLng( 49.07932, 43.12375 );
     var bounds = new google.maps.LatLngBounds(swBound,neBound);
     var imageCode = '<svg width="1800" height="1200" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" class="svg-editor"><g><rect id="svg_5" height="181" width="311" y="95.25" x="47.75" stroke-width="5" fill="#FF0000"/></g></svg>';
     overlay = new myOverlay(bounds, imageCode, map);
@@ -36,6 +36,7 @@ myOverlay.prototype.onAdd = function(){
     div.style.borderWidth = '2px;';
     div.style.background = 'none';
     div.style.position = 'absolute';
+    div.style.color="blue"
     
     var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
     //svg.setAttribute('fill','#FFFFFF');
@@ -43,16 +44,9 @@ myOverlay.prototype.onAdd = function(){
     
     var g = document.createElementNS('http://www.w3.org/2000/svg','g');
     
-    var rect = document.createElementNS('http://www.w3.org/2000/svg','rect');
-    rect.setAttribute('id','myRect');
-    rect.setAttribute('height','181');
-    rect.setAttribute('width','311');
-    rect.setAttribute('y','95.25');
-    rect.setAttribute('x','47.75');
-    rect.setAttribute('stroke-width','5');
-    rect.setAttribute('fill','none');
-    rect.setAttribute('stroke','#FF0000');
-    g.appendChild(rect);
+    // var rect = document.createElementNS('http://www.w3.org/2000/svg','rect');
+
+    // g.appendChild(rect);
     svg.appendChild(g);
     //var img = this.image_;
     div.appendChild(svg);
