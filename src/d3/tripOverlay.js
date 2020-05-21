d49ebb722d1a006bf3f83c67e05685d2bc27e56e
Alexandros Popov <alexandros.ppv@gmail.com>
2020-05-21 10:47:32 +0200

    separated tooltip.

--- src/d3/tripOverlay.js
+++ src/d3/tripOverlay.js
@@ -326,8 +326,6 @@ function setNorthEast( projection,  bounds, padding){
   return ne;
 }
 
-
-
 function printTripInformations( trip ){
 
   var modal = document.getElementById("myModal");
@@ -351,34 +349,5 @@ function printTripInformations( trip ){
     }
   }
 }
-
-function visibleTripTooltip( trip ){
-  d3.select('.tooltip').style("left", (d3.event.pageX + 5) + "px")
-                       .style("top", (d3.event.pageY - 28) + "px")
-                       .html( trip.armyName + "<br>" 
-                              + "De : " + trip.cityNameTripStart + "<br>"
-                              + "Vers : " + trip.cityNameTripEnd + "." )
-                       .transition()
-                       .style("opacity", .9);
-}
-
-function visibleCityTooltip( d, tooltip, tripList ){
-  tooltip.style("left", (d3.event.pageX + 5) + "px")
-         .style("top", (d3.event.pageY - 28) + "px")
-         .html( d.key + "<br>" + d.value.Geographie.Etat )
-         .transition()
-         .style("opacity", .9);
-}
-
-function hideToolTip(){ 
-  d3.select('.tooltip').transition()
-         .style("opacity", 0) 
-         
-}
-
-var tooltip = d3.select( "body" )
-                .append( "div" )
-                .attr( "class", "tooltip" )
-                .style( "opacity", 0);
 
 
