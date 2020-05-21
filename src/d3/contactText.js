89e0908c00d3cdcf9ea0b756eef8382183dc2aa1
Alexandros Popov <alexandros.ppv@gmail.com>
2020-05-21 12:16:41 +0200

    redacted contact text.

---
+++ src/d3/contactText.js
@@ -0,0 +1,21 @@
+function getContactTitle(){
+  return  "Bienvenue dans 'Les croisades pas à pas' !"
+}
+
+function getContactText(){
+  return (
+    "Cette carte interactive vous permet d'explorer à votre aise le trajet des" +
+    " nombreuses armées lors des différents épisodes des Croisades. Vous pouvez sélectionner " +
+    "l'épisode qui vous intéresse le menu supérieur. Actuellement est sélectionné " +
+    "le Parcours des Croisades Populaires initiées par Pierre l'Ermite. Vous pouvez " +
+    "également choisir au sein de chaque épisode les armées dont vous souhaitez voir " +
+    "le parcours et la période temporelle qui vous intéresse. Enfin vous pouvez choisir " +
+    "de masquer les différentes icônes qui symbolisent la nature des points d'arrêts des " +
+    "des armées (Villes, Métropole, Champs, ...) et ce qu'elles y ont fait ( garnison, pillage, ... )." 
+  )
+}
+
+
+function getAuthorText(){
+  return   'Cette outil a été réalisé par : '
+}
